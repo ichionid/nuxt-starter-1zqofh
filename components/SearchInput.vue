7869ff73138601826a813c3302296aa3bf8824e3
ichionid <ichionid@gmail.com>
2023-07-04 14:11:42 +0200

    5 files modified, 2 added

---
+++ components/SearchInput.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Posts</h1>
+    <ul>
+      <li v-for="post in posts" :key="post.id">{{ post.title }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ $http }) {
+    // Make a GET request to the API endpoint
+    const { data: posts } = await $http.get('/api/posts');
+
+    return { posts };
+  },
+};
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,20 @@
-export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss'],
-});
+import { NuxtConfig } from '@nuxt/types';
+
+const config: NuxtConfig = {
+  // Add Tailwind CSS configuration
+  buildModules: ['@nuxt/typescript-build', '@nuxtjs/tailwindcss'],
+
+  // Configure the HTTP module
+  modules: ['@nuxt/http', '@nuxtjs/tailwindcss'],
+  http: {
+    // Enable mocking
+    mock: true,
+  },
+
+  // Define your API mocks
+  serverMiddleware: ['~/mocks/index.js'],
+
+  // Other Nuxt configuration options...
+};
+
+export default config;
