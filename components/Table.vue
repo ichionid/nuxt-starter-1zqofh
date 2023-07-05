43485a9d1113d300db0b5d983a134d689762e049
ichionid <ichionid@gmail.com>
2023-07-05 12:49:45 +0200

    2 files modified, 1 added

---
+++ components/Table.vue
@@ -0,0 +1,32 @@
+<template>
+  <table class="table-fixed flex flex-row border-b border-b-darkgray">
+    <thead v-if="headers">
+      <tr>
+        <th v-for="(header, index) in headers" :key="index">
+          {{ header }}
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(item, index) in items" :key="index">
+        <td v-for="(value, valueIndex) in item" :key="valueIndex">
+          {{ value }}
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+<script>
+export default {
+  props: {
+    items: {
+      type: Array,
+      required: true,
+    },
+    headers: {
+      type: Array,
+      required: false,
+    },
+  },
+};
+</script>
